4f99ee7ab72b44488ed0c15eb719cdded9f7f262
monirulislamriaj <riaj1132@gmail.com>
2020-03-16 12:51:57 +0600

    upded initial upded & chacked responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,56 +1,69 @@
-$(document).ready(function () {
+$(document).ready(function() {
   "use strict";
 
   // Scroll to top
-  $("a[href='#top']").click(function () {
-    $("html, body").animate({
-      scrollTop: 0
-    }, "slow");
+  $("a[href='#top']").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      "slow"
+    );
     return false;
   });
 
   // Smooth scroll
-  $('a.scroll-to').on('click', function (event) {
+  $("a.scroll-to").on("click", function(event) {
     var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    }, 1000);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top - 50
+        },
+        1000
+      );
     event.preventDefault();
     if (screen.width < 992) {
-      $('.navbar-toggler').click();
+      $(".navbar-toggler").click();
     }
   });
 
   // AOS initialize
   AOS.init({
-    disable: 'mobile'
+    disable: "mobile"
   });
 
   // Service Item Match Height
-  $('.service-item').matchHeight({
+  $(".service-item").matchHeight({
     byRow: 0
   });
-  
+
   // Magnific Popup
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+  $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
     disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
+    type: "iframe",
+    mainClass: "mfp-fade",
     removalDelay: 160,
     preloader: false,
     fixedContentPos: false
   });
   // Magnific Gallery
-  $('.gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
+  $(".gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
     closeOnContentClick: false,
     closeBtnInside: false,
-    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    mainClass: "mfp-with-zoom mfp-img-mobile",
     image: {
       verticalFit: true,
-      titleSrc: function (item) {
-        return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.el.attr('data-source') + '" target="_blank">image source</a>';
+      titleSrc: function(item) {
+        return (
+          item.el.attr("title") +
+          ' &middot; <a class="image-source-link" href="' +
+          item.el.attr("data-source") +
+          '" target="_blank">image source</a>'
+        );
       }
     },
     gallery: {
@@ -59,19 +72,18 @@ $(document).ready(function () {
     zoom: {
       enabled: true,
       duration: 300, // don't foget to change the duration also in CSS
-      opener: function (element) {
-        return element.find('img');
+      opener: function(element) {
+        return element.find("img");
       }
     }
-  
   });
 });
 
 // Add nav bg
-$(window).on('scroll', function () {
+$(window).on("scroll", function() {
   if ($(window).scrollTop()) {
-    $('.main-nav').addClass('nav-bg');
+    $(".main-nav").addClass("nav-bg");
   } else {
-    $('.main-nav').removeClass('nav-bg');
+    $(".main-nav").removeClass("nav-bg");
   }
-})
+});
