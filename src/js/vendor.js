2f8c14c10de64469a06ca56e3f3bdd919d7476b6
Md-Saad <md.mayejul@gmail.com>
2021-07-05 19:45:32 +0600

    fixed js links, create privacy page, design related issues

--- src/js/vendor.js
+++
@@ -1,7 +0,0 @@
-@import '../plugins/jQuery/jquery.min.js';
-@import '../plugins/bootstrap/bootstrap.min.js';
-@import '../plugins/slick/slick.min.js';
-@import '../plugins/aos/aos.js';
-@import '../plugins/match-height/match-height.js';
-@import '../plugins/magnific-popup/magnific-popup.min.js';
-@import '../plugins/g-map/gmap.js';
